f100d6000fe426a0c35b8a89becff3a11583ea19
Kseniya38 <shpakovakseniya38@gmail.com>
2022-08-11 16:22:07 +0800

    parametrize and xfile mark added

--- pages/product_page.py
+++ pages/product_page.py
@@ -16,9 +16,9 @@ class ProductPage(BasePage):
     def check_success_message_added_product(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         product_name_in_message = self.browser.find_element(*ProductPageLocators.SUCCESS_ALERT_MESSAGE).text
-        assert product_name == product_name_in_message, f"Product name in message not correct: {product_name_in_message}"
+        assert product_name == product_name_in_message, f"Product name in message not correct: {product_name_in_message} istead of {product_name}"
 
     def check_total_basket_message(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         basket_total_in_message = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL_MESSAGE).text
-        assert product_price == basket_total_in_message, f"Basket total in message not correct: {basket_total_in_message}"
+        assert product_price == basket_total_in_message, f"Basket total in message not correct: {basket_total_in_message} instead of {product_price}"
--- test_product_page.py
+++ test_product_page.py
@@ -1,31 +1,17 @@
-#rom .pages.main_page import MainPage
-#from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 from selenium.webdriver.common.by import By
 import time
+import pytest
 
-#def test_guest_can_go_to_login_page(browser):
-#    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-#    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-#    page.open()                      # открываем страницу
-#    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-#    login_page = LoginPage(browser, browser.current_url)
-#    login_page.should_be_login_page()
-
-#def test_guest_should_see_login_link(browser):
-#    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-#    page = MainPage(browser, link)
-#    page.open()
-#    page.should_be_login_link()
-
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('offer_link', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
+def test_guest_can_add_product_to_basket(browser, offer_link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer_link}"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_cart()
     page.solve_quiz_and_get_code()
-    time.sleep(10)
-    #check_success_message_added_product()
-    #check_total_basket_message()
+    time.sleep(5)
+    page.check_success_message_added_product()
+    page.check_total_basket_message()
 
     
